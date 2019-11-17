e9d0e6fc251839d58d0d64317de6ef33c3a0b9c9
Bryan <shinbryann@gmail.com>
2019-11-17 15:42:32 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -11,20 +11,21 @@ let cards = ["queen", "queen", "king", "king"];
 //empty array to hold cards in play
 cardsInPlay = [];
 
-//create var cardOne
-//store first item in cards array in variable cardOne
-
-let cardOne = cards[0];
-cardsInPlay.push("cardOne");
-
-let cardTwo = cards[2];
-cardsInPlay.push("cardTwo");
-
-//check equality between two cards in cardsInPlay array
-if (cardsInPlay.length === 2) {
-  if (cardOne === cardTwo) {
-    alert("You found a match!");
+function checkForMatch() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!");
   } else {
-    alert("Sorry, try again.");
+    console.log("Sorry, try again.");
   }
 }
+
+function flipCard(cardId) {
+  console.log("User flipped " + cards[cardId]);
+
+  cardsInPlay.push(cards[cardId]);
+
+  checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
