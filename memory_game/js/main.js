ba3a2913f50c6fab981a2e70ce057621de295558
Bryan <shinbryann@gmail.com>
2019-11-17 14:49:28 -0500

    created game loci

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,30 @@
 console.log("up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+//create variables for each card, two are queens, two are kings
+// let cardOne = "queen";
+// let cardTwo = "queen";
+// let cardThree = "king";
+// let cardFour = "king";
 
-console.log("user flipped " + cardOne);
+//array to get rid of each individual variable of cards
+let cards = ["queen", "queen", "king", "king"];
+//empty array to hold cards in play
+cardsInPlay = [];
+
+//create var cardOne
+//store first item in cards array in variable cardOne
+
+let cardOne = cards[0];
+cardsInPlay.push("cardOne");
+
+let cardTwo = cards[2];
+cardsInPlay.push("cardTwo");
+
+//check equality between two cards in cardsInPlay array
+if (cardsInPlay.length === 2) {
+  if (cardOne === cardTwo) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
+}
